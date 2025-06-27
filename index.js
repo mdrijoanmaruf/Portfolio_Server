9a958e246652011ab1ebbb72c4c366045f846cfa
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-28 02:09:23 +0600

    basic server setup

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.get('/' , (req , res) => {
+    res.send("Portfolio Server is Running....")
+})
+
+app.listen(PORT , () => {
+    console.log("Server is Running at port : " , PORT)
+})
